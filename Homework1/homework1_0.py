41ff23f471f950e72f77ad2c86764e21effbf70a
celesteswrath <celesteswrath@mailbox.unideb.hu>
2022-09-19 13:16:03 +0200

    updated homework with functions and organized into folders

--- Homework1/homework1_0.py
+++ Homework1/homework1_0.py
@@ -1,29 +1,43 @@
 #get data from user
-s = input("Adjon meg egy mondatot:")
+def getInput():
+    s = input("Adjon meg egy mondatot:")
+    return s
 
 #count letters
-d = {}
-for i in s:
-    n = 0
-    for j in s:
-        if j == i: n += 1
-    d[i] = n
-print("Betűk gyakorisága:", d)
+def countLetters(sentence):
+    d = {}
+    s = sentence
+    for i in s:
+        n = 0
+        for j in s:
+            if j == i: n += 1
+            d[i] = n
+    print("Betűk gyakorisága:", d)
 
 #reverse string
-r = ""
-for i in range(len(s), 0, -1):
-    r += s[i-1]
-print("Fordítva: ", r)
+def reverse(sentence):
+    s = sentence
+    r = ""
+    for i in range(len(s), 0, -1):
+        r += s[i-1]
+    print("Fordítva: ", r)
 
 #separate words into list
-l = []
-t = ""
-for i in s:
-    if i != " ":
-        t = t + i
-    else:
-        l.append(t)
-        t = ""
-l.append(t)
-print("Listába rendezve szavanként: ", l)
+def separate(sentence):
+    s = sentence
+    l = []
+    t = ""
+    for i in s:
+        if i != " ":
+            t = t + i
+        else:
+            l.append(t)
+            t = ""
+    l.append(t)
+    print("Listába rendezve szavanként: ", l)
+
+if __name__ == "__main__":
+    s = getInput()
+    countLetters(s)
+    reverse(s)
+    separate(s)
--- Homework1/homework1_1.py
+++ Homework1/homework1_1.py
@@ -1,12 +1,20 @@
 #get data from user
-print("Adjon meg egy számot és egy mértékegységet (cm/inch): ")
-n = input()
-s = input()
+def getInput():
+    print("Adjon meg egy számot és egy mértékegységet (cm/inch): ")
+    n = input()
+    s = input()
+    l = [n, s]
+    return l
 
 #convert data
-if s == "cm":
-    print(float(n)*0.3973, " inches")
-elif s == "inch":
-    print(float(n)*2.54, " cms")
-else:
-    print("Not correct unit")
+def convert(n, s):
+    if s == "cm":
+        print(float(n)*0.3973, " inches")
+    elif s == "inch":
+        print(float(n)*2.54, " cms")
+    else:
+        print("Not correct unit")
+
+if __name__ == "__main__":
+    l = getInput()
+    convert(l[0], l[1])
